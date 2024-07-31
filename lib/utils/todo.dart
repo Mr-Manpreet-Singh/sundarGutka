c5de92ff707fcdc9a45b1cb508ebe58e9fd4b523
ManpreetSingh <ms19062001@gmail.com>
2024-07-31 12:26:40 +0530

    hindi path correction

--- lib/utils/todo.dart
+++ lib/utils/todo.dart
@@ -18,49 +18,52 @@
 // custom event added to track user_settings in shared prefrences file... check the analytics dashboard tomorrow
 //  path errors found my mummy
   // - page 162 (18 -> 6)word nirmal page 10(8 ->1) sukhmani sahib, 7th astpati 3rd poudi (pg58) send to mummy for verification
-  //D - Tav-Parsad Savaiye starting  ੴ ਸਤਿਗੁਰ ਪ੍ਰਸਾਦਿ ਪਾਤਸਾਹੀ ॥੧੦॥
-  //D - ReshrasSahib starting        ਰਹਿਰਾਸ ਸਾਹਿਬ
-  //D - ReshrasSahib page 43
-  //D - Kirtan Sohila page 4 
-  //D - ardas page 6 
-// D chaupai sahib in hindi left
-//D hindi paths satrting left
-//D hindi sayaiye word left
-//D punjabi errors of sukhmani sahib left
-//D when we change the language how we will deal with the index of page  try catch 
-//D add alanlytics .. 
+  // - Tav-Parsad Savaiye starting  ੴ ਸਤਿਗੁਰ ਪ੍ਰਸਾਦਿ ਪਾਤਸਾਹੀ ॥੧੦॥
+  // - ReshrasSahib starting        ਰਹਿਰਾਸ ਸਾਹਿਬ
+  // - ReshrasSahib page 43
+  // - Kirtan Sohila page 4 
+  // - ardas page 6 
+// chaupai sahib in hindi left
+// hindi paths satrting left
+// hindi sayaiye word left
+// punjabi errors of sukhmani sahib left
+// when we change the language how we will deal with the index of page  try catch 
+// add alanlytics .. 
 // seeting Switch to shoe bottom buttons on page view
 // select text and report feature || Firebase unsafe error
 // ->  | Mail | Rate | Share 
 // Send Feedback
 // snackbar for internet ..
 // how to report error / correction
-
-
-  // TODO ! mark in path HINDI ardas
-  // singh apps $5.18 space ship
-  // Developer name choose ... == Singh APps or Singh Dev Studio
-  //  App name Gutka Sahib ( play store only..No Ads) 
-  //APP logo-> Name ->splash screen(logo, title, head Cover) -> About (version + created by) -> Link Privacy Policy to website
-  // Make website as to show case portfolio
-  //WEB Nav bar not working properly + Mobile nav bar -> Text Edit -> scroll id hard ->
-
+  //APP logo-> Name ->splash screen(logo, title, head Cover) -> -> Link Privacy Policy to website
+  //  ! mark in path HINDI ardas
  // About  
   // Cover your head screen + app logo
-// google account creation
 //  app logo | name | App bat Title
-//  crah analytics
-// before publishing secure the api keys ... settings file in google ...
-//  google remote config.. 
-//  Read about app versions
 
-//  max in starting i can make it 50 max in 24hrs req from a person for feedback in order to save api....
+
+// TODO
+ 
+  // Developer name choose ... == Singh APps or Singh Dev Studio
+  // google account creation
+  //  app mock ups
+  // app discription
+  // app data disclosure form
+
+
+
+  // singh apps $5.18 space ship
+  //  App name Gutka Sahib ( play store only..No Ads) 
+  // Make website as to show case portfolio
+  //WEB Nav bar not working properly + Mobile nav bar -> Text Edit -> scroll id hard ->
+// before publishing secure the api keys ... settings file in google ...
+
+
 // package_info_plus 8.0.0 for version in app screen
   // URL Launcher
     //    -> Privacy policy web page  // use google sites for the same or download any site  and Terms of use
     // Terms of use
   // DATA DISCOLosures
-    // https://firebase.google.com/support/privacy/dpo
     // https://firebase.google.com/docs/android/play-data-disclosure
     // Rayat tech
     //    -> Read ME how to use app
@@ -69,6 +72,11 @@
     //    -> collected states feature to enable disable alanytics
     
 // TODO Version 2
+//  max in starting i can make it 50 max in 24hrs req from a person for feedback in order to save api....
+//  crah analytics
+//  google remote config.. 
+//  Read about app versions
+//  privacy policy on web site
 // share | Rate URL
     //    -> More apps -> popup Play store -> searched pub:____
 // add english
